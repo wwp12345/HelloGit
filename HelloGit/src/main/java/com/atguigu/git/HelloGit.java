8f71f036eb2b9491a2032c50b2f489b45e569433
WangWeiPeng <1474693016@qq.com>
2020-12-01 17:21:51 +0800

    主干添加

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit");
         System.out.println("分支");
+        System.out.println("主干添加");
     }
 }
